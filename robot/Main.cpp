c4cfb6322def2bac36ee245d64d94e8ade9eebb1
Rob Ekelmans <rob.ekelmans@philips.com>
2023-11-08 14:59:27 +0000

    chore: Create PWM driver and use it in example application

--- robot/Main.cpp
+++ robot/Main.cpp
@@ -1,117 +1,67 @@
 #include "hal_st/instantiations/NucleoTracerInfrastructure.hpp"
 #include "hal_st/instantiations/NucleoUi.hpp"
 #include "hal_st/instantiations/StmEventInfrastructure.hpp"
-#include "hal_st/stm32fxxx/DefaultClockNucleoF767ZI.hpp"
-#include "infra/timer/Timer.hpp"
-// #include "services/util/DebugLed.hpp"
-#include <chrono>
-#include <string>
-using namespace std;
-unsigned int hse_value = 8000000;
+#include "hal_st/stm32fxxx/DefaultClockNucleoWB55RG.hpp"
+#include "services/util/DebugLed.hpp"
+#include "robot_services/ShiftRegisterDriver.hpp"
+#include "robot/PwmDriver.hpp"
 
-uint8_t ledCyclus;
-
-string intToByteString(uint8_t& ledCyclus)
-{
-    string byteString = "";
-    for (int i = 7; i >= 0; i--)
-    {
-        int b = ledCyclus >> i;
-        if (b & 1)
-            byteString += "1";
-        else
-            byteString += "0";
-    };
-    return byteString;
-}
+unsigned int hse_value = 32000000;
 
 int main()
 {
     HAL_Init();
 
     // Configure your clock here
-    ConfigureDefaultClockNucleo767ZI();
+    ConfigureDefaultClockNucleoWB55RG();
 
     static main_::StmEventInfrastructure eventInfrastructure;
-    static main_::Nucleo144Ui ui;
-    // static services::DebugLed debugLed(ui.ledBlue);;
-    static hal::OutputPin ledRed(ui.ledRed);
-    static hal::OutputPin ledGreen(ui.ledGreen);
-    static hal::OutputPin ledBlue(ui.ledBlue);
+    static main_::Nucleo64WBUi ui;
+    static services::DebugLed debugLed(ui.ledBlue);
 
-    // static hal::InputPin button(ui.buttonOne);
+    static main_::NucleoWb55rgTracerInfrastructure tracer;
 
-    static main_::NucleoF767ziTracerInfrastructure tracer;
+    tracer.tracer.Trace() << "hello world!";
 
-    // tracer.tracer.Trace() << str;
+    static hal::GpioPinStm latch(hal::Port::A, 6);
+    static hal::GpioPinStm clock(hal::Port::C, 10);
+    static hal::GpioPinStm serialIn(hal::Port::C, 12);
+    static hal::GpioPinStm enable(hal::Port::C, 13);
 
-    static infra::TimerRepeating timer(std::chrono::seconds(1), []()
+    static ShiftRegisterDriver shiftRegisterDriver(enable, latch, clock, serialIn);
+
+    shiftRegisterDriver.ShiftByte(0b11'00'00'00);
+    shiftRegisterDriver.DisableOutput();
+
+    __HAL_RCC_TIM1_CLK_ENABLE();
+    __HAL_RCC_TIM2_CLK_ENABLE();
+
+    static hal::GpioPinStm pwmMotorFourPin(hal::Port::A, 8, hal::Drive::PushPull, hal::Speed::High, hal::WeakPull::Up);
+    static hal::PeripheralPinStm pwmMotorFour(pwmMotorFourPin, hal::PinConfigTypeStm::timerChannel1, 1);
+    static hal::GpioPinStm pwmMotorThreePin(hal::Port::A, 15, hal::Drive::PushPull, hal::Speed::High, hal::WeakPull::Up);
+    static hal::PeripheralPinStm pwmMotorThree(pwmMotorThreePin, hal::PinConfigTypeStm::timerChannel1, 2);
+
+    static PwmDriver PwmDriver1(TIM1);
+    static PwmDriver PwmDriver2(TIM2);
+
+    PwmDriver1.SetDutyCycle(0);
+    PwmDriver2.SetDutyCycle(0);
+
+    ui.buttonOne.Config(hal::PinConfigType::input);
+    ui.buttonOne.EnableInterrupt([]()
         {
+            tracer.tracer.Trace() << "BU1: pressed";
+            PwmDriver1.SetDutyCycle(50);
+        },
+        hal::InterruptTrigger::fallingEdge);
 
-            ledCyclus++;
-            if (ledCyclus == 8)
-            {
-                int blinkCounter = 0;
-                unsigned long currentTime;
-                unsigned long previousTime;
-                bool ledsOn = false;
-                while (true) {
-                    if (blinkCounter >= 10) {
-                        break;
-                    }
-                    currentTime = HAL_GetTick();
-                    if (currentTime - previousTime >= 50) {
-                        blinkCounter++;
-                        if (ledsOn) {
-                            ledRed.Set(false);
-                            ledGreen.Set(false);
-                            ledBlue.Set(false);
-                            ledsOn = false;
-                        } else {
-                            ledRed.Set(true);
-                            ledGreen.Set(true);
-                            ledBlue.Set(true);
-                            ledsOn = true;
-                        }
-                        previousTime = currentTime;
-                        
-                    }
-                }
-                ledCyclus = 0;
-            }
-            string byteString = intToByteString(ledCyclus);
-
-            if (byteString.at(5) == '1')
-            {
-                ledGreen.Set(true);
-            }
-            else
-            {
-                ledGreen.Set(false);
-            };
-
-            if (byteString.at(6) == '1')
-            {
-                ledBlue.Set(true);
-            }
-            else
-            {
-                ledBlue.Set(false);
-            };
-
-            if (byteString.at(7) == '1')
-            {
-                ledRed.Set(true);
-            }
-            else
-            {
-                ledRed.Set(false);
-            };
-        });
-
-    // static hal::InputPin button(ui.buttonOne);
-
-  
+    ui.buttonTwo.Config(hal::PinConfigType::input);
+    ui.buttonTwo.EnableInterrupt([]()
+        {
+            tracer.tracer.Trace() << "BU2: pressed";
+            PwmDriver2.SetDutyCycle(50);
+        },
+        hal::InterruptTrigger::fallingEdge);
 
     eventInfrastructure.Run();
     __builtin_unreachable();
--- robot/PwmDriver.hpp
+++ robot/PwmDriver.hpp
@@ -1,15 +1,19 @@
 #ifndef PWM_DRIVER_HPP
 #define PWM_DRIVER_HPP
 
-#include "hal/interfaces/Gpio.hpp"
+#include DEVICE_HEADER
+#include "stm32wbxx_hal_tim.h"
 #include <cstdint>
 
 class PwmDriver
 {
 public:
-    PwmDriver(hal::PinConfigType config, hal::GpioPin& pin);
+    PwmDriver(TIM_TypeDef* timer);
 
     void SetDutyCycle(uint8_t dutyCycle);
+
+private:
+    TIM_HandleTypeDef handle;
 };
 
 #endif
