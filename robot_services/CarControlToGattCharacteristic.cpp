9408363bef3baa8fc6037661b3cd96ab7dd37788
semerded <sem.van.broekhoven@gmail.com>
2023-11-13 15:23:06 +0000

    initialization test

--- robot_services/CarControlToGattCharacteristic.cpp
+++ robot_services/CarControlToGattCharacteristic.cpp
@@ -1,6 +1,13 @@
 #include "robot_services/CarControlToGattCharacteristic.hpp"
 
-
 CarControlToGattCharacteristic::CarControlToGattCharacteristic(CarControl& carControl, RobotServiceGattServer& robotServiceGattServer)
     : carControl(carControl)
-    , robotServiceGattServer(robotServiceGattServer){};
+    , robotServiceGattServer(robotServiceGattServer)
+{
+    robotServiceGattServer.SpeedLeft();
+    robotServiceGattServer.SpeedRight();
+    robotServiceGattServer.DirectionLeft();
+    robotServiceGattServer.DirectionRight();
+    robotServiceGattServer.Stop();
+    robotServiceGattServer.Ack();
+}
