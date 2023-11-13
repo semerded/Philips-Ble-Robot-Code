5ee18c97198f5afdd5d5baefcca036f27d43d81b
semerded <sem.van.broekhoven@gmail.com>
2023-11-13 15:04:52 +0000

    test for speed left in CarControlToGattCharacteristic

--- robot_services/test/TestCarControlToGattCharacteristic.cpp
+++ robot_services/test/TestCarControlToGattCharacteristic.cpp
@@ -1,7 +1,10 @@
+#include "infra/util/Function.hpp"
+#include "infra/util/MemoryRange.hpp"
 #include "robot_services/CarControlToGattCharacteristic.hpp"
 #include "robot_services/test_doubles/CarControlMock.hpp"
 #include "robot_services/test_doubles/RobotServiceGattServerMock.hpp"
 #include "gmock/gmock.h"
+#include <cstdint>
 #include <gtest/gtest.h>
 
 class CarControlToGattCharacteristicTest
@@ -16,3 +19,14 @@ class CarControlToGattCharacteristicTest
 
 TEST_F(CarControlToGattCharacteristicTest, initialization)
 {}
+
+
+TEST_F(CarControlToGattCharacteristicTest, speedLeft) 
+{
+    uint8_t percentage = 50;
+    EXPECT_CALL(carControl, speedMotorLeft(percentage, testing::_));
+    robotServiceGattServer.SpeedLeft().NotifyObservers([percentage](auto& observer){
+
+        observer.DataReceived(infra::MakeRangeFromSingleObject(percentage) );
+    });
+}
